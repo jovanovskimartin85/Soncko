e913b16b5c4079db02c88c2f85a095b4a46c0572
Martin Jovanovski <jovanovskimartin85@gmail.com>
2019-04-06 23:07:16 +0200

    Changes in the first project

---
+++ First Project/src/Calculator.java
@@ -0,0 +1,22 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		
+		int age = 33;
+		String name = "Dime";
+		int inteligence = 183;
+		
+		
+		if (inteligence < 120 ) {
+			System.out.println(name + " is dumb as shit");
+			
+		}	else {
+			System.out.println(name + " might be as smart as Martin");
+		}
+		
+
+
+	}
+
+}
